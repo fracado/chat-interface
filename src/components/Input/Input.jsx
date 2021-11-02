5d3d374e5a79eb0d83e498e6fa88e99c44092435
Franziska Bath <franziska.bath@instafreight.com>
2021-11-02 22:09:00 +0100

    add input container

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+const Input = () => {
+    const [inputValue, setInputValue] = useState('');
+
+    return (
+        <footer className="input-container">
+            <form>
+
+            </form>
+        </footer>
+    )
+};
+
+export default Input;
